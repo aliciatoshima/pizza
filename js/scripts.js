716a37c92dd04430448a14f892077d188d6a80cc
Alicia Toshima and Karla Carballo <alicia@>
2019-03-13 21:01:31 -0700

    we did it

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business Logic for AddressBook ---------
 function AddressBook() {
-  this.contacts = []
+  this.contacts = [];
 }
 
 AddressBook.prototype.addContact = function(contact) {
@@ -17,7 +17,7 @@ AddressBook.prototype.addContact = function(contact) {
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
-  this.addresses = [];
+  this.address = [];
 }
 
 function Address(street, city, state) {
@@ -41,6 +41,24 @@ var addressBook1 = new AddressBook();
 
 $(document).ready(function(){
 
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group" id="input4">' +
+                                   '<label for="street">Street</label>' +
+                                   '<input type="text" id="street" class="form-control">' +
+                                 '</div>' +
+                                 '<div class="form-group"id="input5">' +
+                                   '<label for="city">City</label>' +
+                                   '<input type="text" id="city" class="form-control">' +
+                                 '</div>' +
+                                 '<div class="form-group"id="input6">' +
+                                   '<label for="state">State</label>' +
+                                   '<input type="text" id="state" class="form-control">' +
+                                 '</div>' +
+                               '</div>');
+
+    });
+
   $('form#inputform').submit(function(event){
     event.preventDefault();
 
@@ -50,24 +68,60 @@ $(document).ready(function(){
     newContact.lastName = $('#lname').val();
     //newContact.phoneNumber = $('#pnum').val();
 
-    var newAddress = new Address();
-    newAddress.street = $('#street').val();
-    newAddress.city = $('#city').val();
-    newAddress.state = $('#state').val();
+    // var newAddress = new Address();
+    // newAddress.street = $('#street').val();
+    // newAddress.city = $('#city').val();
+    // newAddress.state = $('#state').val();
+
+    $(".new-address").each(function() {
+      //var newAddress = new Address();
+      //debugger;
+      var newStreet = $(this).find("input#street").val();
+      var newCity = $(this).find("input#city").val();
+      var newState = $(this).find("input#state").val();
+      var newAddress = new Address(newStreet, newCity, newState);
+      newContact.address.push(newAddress);
+    });
+
 
 
     var fname = newContact.fullName();
 
-    console.log(newContact);
+    //console.log(newContact);
 
     addressBook1.contacts.push(newContact);
+    //newContact.address.push(newAddress);
 
-    console.log(addressBook1[0]);
+    //console.log(addressBook1[0]);
 
     // $('#contactlist').append(`<li>Name: ${fname}, Phone Number: ${newContact.phoneNumber}, </li>`).hide().fadeIn();
-    $('#contactlist').append(`<li>Name: ${fname}, Address: ${newAddress.fullAddress()} </li>`).hide().fadeIn();
+    $('#contactlist').append(`<li><span class='contact'>Name: ${fname} </span></li>`).hide().fadeIn();
+
+    $(".contact").last().click(function() {
+      //debugger;
+      $("#show-contact").show();
+      $("#show-contact h2").text(fname);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      //$("ul#addresses").append(`<li> ${newAddress.fullAddress()} </li>`);
+      $("ul#addresses").text("")
+      // var test = newContact.address[0];
+      // return test
+      newContact.address.forEach(function(address1) {
+        //debugger;
+        $("ul#addresses").append("<li>" + address1.fullAddress() + "</li>");
+    });
+
+   //  $("input#fname").val("");
+   // $("input#lname").val("");
+   // $("input#street").val("");
+   // $("input#city").val("");
+   // $("input#state").val("");
+
 
   });
 
+
+});
 
 });
