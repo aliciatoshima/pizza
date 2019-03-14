7b49f99e2a3c32e45b0d6bcbef309fbc650b4ae0
Alicia Toshima and Karla Carballo <karla@>
2019-03-13 18:15:28 -0700

    cloned address book and made a few changes

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,24 @@ AddressBook.prototype.addContact = function(contact) {
 }
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.phoneNumber = phoneNumber
+// function Contact(firstName, lastName, phoneNumber) {
+//   this.firstName = firstName,
+//   this.lastName = lastName,
+//   this.phoneNumber = phoneNumber
+// }
+
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.addresses = [];
 }
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
