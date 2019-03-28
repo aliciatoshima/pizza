c739f3fabbbe6cf87eefcadae0324dc00841ffeb
Alicia Toshima and Karla Carballo <karla@>
2019-03-27 19:38:22 -0700

    the pig dice might be done however it is quite lacking in terms of visual aesthetics

--- js/scripts.js
+++ js/scripts.js
@@ -36,9 +36,15 @@ var img = new Image();
       Player1.current_total = 0
       $(".p1-roll").empty()
       alert("Player 2's turn!")
+      $(".img").empty()
     } else {
-      //debugger
-      //console.log(Player1.current_total)
+      img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_" +roll1 + "-512.png";
+      img.alt = "dice2";
+      img.style.width = "300px";
+
+      $(".img").empty().append(img);
+      $(".img").show()
+
      Player1.current_total = Player1.current_total + roll1
     }
 
@@ -55,46 +61,18 @@ var img = new Image();
         Player2.current_total = 0
         $(".p2-roll").empty()
         alert("Player 1's turn!")
+        $(".img").empty()
       } else {
-        if (roll2 === 2) {
-          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_2-512.png";
-          img.alt = "dice2";
-          img.style.width = "300px";
 
-          $(".img").empty().append(img);
-          $(".img").show()
-        } else if (roll2 === 3) {
-          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_3-512.png";
-          img.alt = "dice3";
-          img.style.width = "300px";
+        img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_" +roll2 + "-512.png";
+        img.alt = "dice2";
+        img.style.width = "300px";
 
-          $(".img").empty().append(img);
-          $(".img").show()
-        } else if (roll2 === 4) {
-          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_4-512.png";
-          img.alt = "dice4";
-          img.style.width = "300px";
-
-          $(".img").empty().append(img);
-          $(".img").show()
-        } else if (roll2 === 5) {
-          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_5-512.png";
-          img.alt = "dice5";
-          img.style.width = "300px";
-
-          $(".img").empty().append(img);
-          $(".img").show()
-        } else if (roll2 === 6) {
-          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_6-512.png";
-          img.alt = "dice6";
-          img.style.width = "300px";
-
-          $(".img").empty().append(img);
-          $(".img").show()
-        }
+        $(".img").empty().append(img);
+        $(".img").show()
 
 
-       Player2.current_total = Player2.current_total + roll2;
+        Player2.current_total = Player2.current_total + roll2;
       }
 
       $(".p2-current").text(Player2.current_total)
@@ -112,8 +90,10 @@ var img = new Image();
       //alert("Player 1 has won!")
       $(".who-won").text("1")
       $(".winner").show()
+      $(".reset").show()
     } else {
       alert("Player 2's turn!")
+      $(".img").empty()
     }
   });
 
@@ -126,18 +106,22 @@ var img = new Image();
     $(".p2-total").text(Player2.total)
     //p2-roll").empty()
     Player2.current_total = 0
-    //alert("Player 1's turn!")
 
     if (Player2.total >= 100) {
       //alert("Player 1 has won!")
       $(".who-won").text("2")
       $(".winner").show()
+      $(".reset").show()
     } else {
       alert("Player 1's turn!")
+      $(".img").empty()
     }
 
   });
 
+  $("#reset").click(function() {
+    location.reload();
+  });
 
 
 });
