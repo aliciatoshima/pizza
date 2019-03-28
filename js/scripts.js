efc7320836d00ed7ce7762584d3f928db901560d
Alicia Toshima and Karla Carballo <alicia@>
2019-03-27 19:08:17 -0700

    have a working page with a dice that shows up

--- js/scripts.js
+++ js/scripts.js
@@ -18,19 +18,16 @@ var current_total = 0
 var total = 0
 
 $(document).ready(function(){
-// var current_total = 0
-//
-//
-// var total = 0
-
 
 var Player1 = new Player(total, current_total);
 var Player2 = new Player(total, current_total);
+var img = new Image();
 
   // when roll is clicked, show the roll of the dice
   //console.log(Player1.current_total)
 
   $("#p1-roll").click(function() {
+
     //debugger;
     roll1 = randInt(1,7);
     $(".p1-roll").text(roll1)
@@ -38,7 +35,7 @@ var Player2 = new Player(total, current_total);
     if (roll1 === 1) {
       Player1.current_total = 0
       $(".p1-roll").empty()
-      alert("Other player's turn!")
+      alert("Player 2's turn!")
     } else {
       //debugger
       //console.log(Player1.current_total)
@@ -52,16 +49,52 @@ var Player2 = new Player(total, current_total);
     $("#p2-roll").click(function() {
       //debugger;
       roll2 = randInt(1,7);
-      $(".p2-roll").text(roll1)
+      $(".p2-roll").text(roll2)
 
       if (roll2 === 1) {
         Player2.current_total = 0
         $(".p2-roll").empty()
-        alert("Other player's turn!")
+        alert("Player 1's turn!")
       } else {
-        //debugger
-        //console.log(Player1.current_total)
-       Player2.current_total = Player2.current_total + roll2
+        if (roll2 === 2) {
+          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_2-512.png";
+          img.alt = "dice2";
+          img.style.width = "300px";
+
+          $(".img").empty().append(img);
+          $(".img").show()
+        } else if (roll2 === 3) {
+          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_3-512.png";
+          img.alt = "dice3";
+          img.style.width = "300px";
+
+          $(".img").empty().append(img);
+          $(".img").show()
+        } else if (roll2 === 4) {
+          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_4-512.png";
+          img.alt = "dice4";
+          img.style.width = "300px";
+
+          $(".img").empty().append(img);
+          $(".img").show()
+        } else if (roll2 === 5) {
+          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_5-512.png";
+          img.alt = "dice5";
+          img.style.width = "300px";
+
+          $(".img").empty().append(img);
+          $(".img").show()
+        } else if (roll2 === 6) {
+          img.src = "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_6-512.png";
+          img.alt = "dice6";
+          img.style.width = "300px";
+
+          $(".img").empty().append(img);
+          $(".img").show()
+        }
+
+
+       Player2.current_total = Player2.current_total + roll2;
       }
 
       $(".p2-current").text(Player2.current_total)
@@ -74,7 +107,17 @@ var Player2 = new Player(total, current_total);
     $(".p1-total").text(Player1.total)
     //p2-roll").empty()
     Player1.current_total = 0
+
+    if (Player1.total >= 100) {
+      //alert("Player 1 has won!")
+      $(".who-won").text("1")
+      $(".winner").show()
+    } else {
+      alert("Player 2's turn!")
+    }
   });
+
+
 
   $("#p2-hold").click(function() {
 
@@ -83,62 +126,18 @@ var Player2 = new Player(total, current_total);
     $(".p2-total").text(Player2.total)
     //p2-roll").empty()
     Player2.current_total = 0
+    //alert("Player 1's turn!")
+
+    if (Player2.total >= 100) {
+      //alert("Player 1 has won!")
+      $(".who-won").text("2")
+      $(".winner").show()
+    } else {
+      alert("Player 1's turn!")
+    }
+
   });
 
-  // when hold is clicked, return the sum of the rolls for the turn
-  // also return the total sum
-  // $('#p1-hold').click(function() {
-  //   event.preventDefault();
 
-
-    // var newContact = new Contact();
-    // newContact.firstName = $('#fname').val();
-    // newContact.lastName = $('#lname').val();
-    // //newContact.phoneNumber = $('#pnum').val();
-    //
-    // // var newAddress = new Address();
-    // // newAddress.street = $('#street').val();
-    // // newAddress.city = $('#city').val();
-    // // newAddress.state = $('#state').val();
-    //
-    // $(".new-address").each(function() {
-    //   //var newAddress = new Address();
-    //   //debugger;
-    //   var newStreet = $(this).find("input#street").val();
-    //   var newCity = $(this).find("input#city").val();
-    //   var newState = $(this).find("input#state").val();
-    //   var newAddress = new Address(newStreet, newCity, newState);
-    //   newContact.address.push(newAddress);
-    // });
-    //
-    //
-    //
-    // var fname = newContact.fullName();
-    //
-    //
-    //
-    // addressBook1.contacts.push(newContact);
-    //
-    // $('#contactlist').append(`<li><span class='contact'>Name: ${fname} </span></li>`).hide().fadeIn();
-    //
-    // $(".contact").last().click(function() {
-    //   //debugger;
-    //   $("#show-contact").show();
-    //   $("#show-contact h2").text(fname);
-    //   $(".first-name").text(newContact.firstName);
-    //   $(".last-name").text(newContact.lastName);
-    //   //$("ul#addresses").append(`<li> ${newAddress.fullAddress()} </li>`);
-    //   $("ul#addresses").text("")
-    //   // var test = newContact.address[0];
-    //   // return test
-    //   newContact.address.forEach(function(address1) {
-    //
-    // });
-
-
-  // });
-
-
-// });
 
 });
